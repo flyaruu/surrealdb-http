1f01b7e78c4374986b98407093b022bb902d5f45
Frank Lyaruu <f.lyaruu@quin.nl>
2023-07-10 14:29:54 +0200

    use more high level api, add some tests

--- src/surreal.rs
+++ src/surreal.rs
@@ -1,8 +1,9 @@
 
-use std::{error::Error, fmt::Display};
+use std::{error::Error, fmt::Display, str::from_utf8};
 
 use base64::{Engine as _, engine::general_purpose};
-use serde_json::Value;
+use serde::{Deserialize};
+use serde_json::{Value, from_value, Map};
 use simplehttp::simplehttp::SimpleHttpClient;
 
 pub struct SurrealDbClient {
@@ -15,6 +16,47 @@ pub struct SurrealDbClient {
 
 #[derive(Debug)]
 pub struct SurrealDbError(String, Option<Box<dyn Error>>);
+
+#[derive(Deserialize,Debug,PartialEq, Eq)]
+pub enum SurrealStatus {
+    OK,
+    ERROR,
+}
+#[derive(Deserialize,Debug)]
+pub struct DynamicSurrealResult (Vec<DynamicSurrealStatementReply>);
+
+impl DynamicSurrealResult {
+    pub fn take_first(mut self)->Result<DynamicSurrealStatementReply,SurrealDbError> {
+        Ok(self.0.pop().ok_or(SurrealDbError("Missing result field".to_owned(), None))?)
+    }    
+}
+#[derive(Deserialize,Debug)]
+pub struct DynamicSurrealStatementReply {
+    status: SurrealStatus,
+    result: Option<Vec<Value>>,
+}
+
+impl DynamicSurrealStatementReply {
+    pub fn take_first(self)->Result<Value,SurrealDbError> {
+        if self.status != SurrealStatus::OK {
+            return Err(SurrealDbError(format!("Error reported in reply:{:?}",self.status), None))
+        }
+        let ll = self.result
+            .ok_or(SurrealDbError("Missing result field".to_owned(), None))?
+            .pop();
+            
+        ll.ok_or(SurrealDbError("Empty result field".to_owned(),None))
+    }
+}
+
+#[derive(Deserialize,Debug)]
+pub struct SurrealResult<T> (Vec<SurrealStatementReply<T>>);
+#[derive(Deserialize,Debug)]
+pub struct SurrealStatementReply<T> {
+    status: SurrealStatus,
+    result: Vec<T>,
+}
+
 
 impl SurrealDbError {
     fn new(message: &str, cause: Option<Box<dyn Error>>)->Self {
@@ -42,11 +84,10 @@ impl Error for SurrealDbError {
 }
 
 impl SurrealDbClient {
+    // Add builder pattern?
     pub fn new(username: &str, password: &str, base_url: &str, namespace: &str, database: &str, client: Box<dyn SimpleHttpClient>)->Self {
-        // let token = base64::Engine::
         let mut auth_token = String::new();
         general_purpose::STANDARD.encode_string(format!("{}:{}",username,password), &mut auth_token);
-
         Self { auth_token, base_url: base_url.to_owned(), namespace: namespace.to_owned(), database: database.to_owned(), client}
     }
 
@@ -63,7 +104,26 @@ impl SurrealDbClient {
         Ok(result)
     }
 
-    pub fn insert(&mut self, table: &str, key: Option<&str>, value: &[u8])->Result<Vec<u8>, SurrealDbError> {
+    pub fn delete(&mut self, table: &str, key: Option<&str>)->Result<Vec<u8>, SurrealDbError> {
+        let headers = [
+            ("DB",self.database.as_str()),
+            ("NS",self.namespace.as_str()),
+            ("Accept","application/json"),
+            ("Authorization",&format!("Basic {}",self.auth_token))
+        ];
+
+        let url = match key {
+            Some(key)=>format!("{}/key/{}/{}",self.base_url,table,key),
+            None => format!("{}/key/{}",self.base_url,table),
+        };
+    
+        let result = self.client.delete(&url, &headers[..])
+            .map_err(|e| SurrealDbError::new(&format!("Error getting table: {} id: {:?}",table,key),Some(Box::new(e))))?;
+        Ok(result)
+    }
+
+    // DynamicSurrealResult
+    pub fn insert(&mut self, table: &str, key: Option<&str>, value: &[u8])->Result<DynamicSurrealResult, SurrealDbError> {
         let headers = [
             ("DB",self.database.as_str()),
             ("NS",self.namespace.as_str()),
@@ -76,15 +136,27 @@ impl SurrealDbClient {
         };
         let inserted = self.client.post(&url, &headers,value)
             .map_err(|e| SurrealDbError::new(&format!("Error querying table: {} key: {:?}",table,key),Some(Box::new(e))))?;
-        Ok(inserted)
+        let parsed: Value = serde_json::from_slice(&inserted)
+            .map_err(|e| SurrealDbError::new(&format!("Error parsing json result from insert at table: {} key: {:?}",table, key),Some(Box::new(e))))?;
+
+        let l = from_value::<DynamicSurrealResult>(parsed)
+            .map_err(|e| SurrealDbError::new(&format!("Error interpreting json result from insert at table: {} key: {:?}",table, key),Some(Box::new(e))))?;
+
+        Ok(l)
     }
 
     pub fn insert_for_id(&mut self, table: &str, value: &[u8])->Result<String, SurrealDbError> {
-        let res = self.insert(table, None, value)?;
-        let v: Value = serde_json::from_slice(&res).unwrap();
-        let first_result = v.as_array().unwrap().first().unwrap().as_object().unwrap().get("result").unwrap().as_array().unwrap().first().unwrap();
-        let id = first_result.get("id").unwrap().as_str().unwrap().to_owned();
-        Ok(id)
+        let res = self.insert(table, None, value)?
+            .0
+            .pop()
+            .ok_or(SurrealDbError("Missing result element".to_owned(),None))?
+            .take_first()?;
+
+        res.get("id")
+            .ok_or(SurrealDbError("Missing id".to_owned(),None))?
+            .as_str()
+            .ok_or(SurrealDbError("Bad id".to_owned(),None))
+            .map(|e|e.to_owned())
     }
 
     pub fn is_healthy(&mut self)->bool {
@@ -107,14 +179,42 @@ impl SurrealDbClient {
             .map_err(|e| SurrealDbError::new(&format!("Error querying: {}",query),Some(Box::new(e))))?;
         Ok(result)
     }
+
+    pub fn query_dynamic_single(&mut self, query: &str)->Result<DynamicSurrealResult,SurrealDbError> {
+        let value = self.query(query)?;
+        let v: Value = serde_json::from_slice(&value)
+            .map_err(|e| SurrealDbError::new(&format!("Error parsing json result: {}",query),Some(Box::new(e))))?;
+        // let l = v.as_array().unwrap().first().unwrap();
+        // println!("Thing: {:?}",l);
+        let l = from_value::<DynamicSurrealResult>(v)
+            .map_err(|e| SurrealDbError::new(&format!("Error parsing json result: {}",query),Some(Box::new(e))))?;
+        Ok(l)
+    }
+    pub fn query_single<T>(&mut self, query: &str)->Result<SurrealResult<T>,SurrealDbError> where T: for<'a> Deserialize<'a> {
+        let value = self.query(query)?;
+        let value_string = from_utf8(&value).unwrap();
+        println!("{}",value_string);
+        let result: SurrealResult<T> = serde_json::from_slice(&value)
+            .map_err(|e| SurrealDbError::new(&format!("Error parsing json result: {}",query),Some(Box::new(e))))?;
+        Ok(result)
+    }
+    // pub fn query_single<'de, T> where T: Deserialize<'de> (&mut self, query: &str)->Result<SurrealResult<T>,SurrealDbError> where T: Deserialize {
+    //     let value = self.query(query)?;
+    //     let result: SurrealResult<T> = serde_json::from_slice(&value)
+    //         .map_err(|e| SurrealDbError::new(&format!("Error parsing json result: {}",query),Some(Box::new(e))))?;
+    //     Ok(result)
+    // }
 }
 
 #[cfg(test)]
 mod test {
     use std::{str::from_utf8};
 
+    use serde::Deserialize;
     use serde_json::Value;
     use simplehttp::{simplehttp_reqwest::SimpleHttpClientReqwest};
+    use crate::surreal::SurrealResult;
+
     use super::SurrealDbClient;
 
     fn create_test_client()->SurrealDbClient {
@@ -131,7 +231,7 @@ mod test {
     #[test]
     fn test_query() {
         let mut surreal = create_test_client();
-        let res = surreal.query("select * from table").unwrap();
+        let res = surreal.query("select * from person").unwrap();
         let res = from_utf8(&res).unwrap();
         println!("Result: {}",res);
     }
@@ -139,16 +239,19 @@ mod test {
     #[test]
     fn test_insert() {
         let mut surreal = create_test_client();
-        let example = r#"{"kip":{"aap":"sji"},"aap":{"mies":"sjo"}}"#;
+        // surreal.del
+        let example = r#"{"kip":{"aap":"sji"},"aap":{"mies":"sjo"}}"#.as_bytes();
         for _ in 0..10 {
-            let res = surreal.insert("test_table", None, example.as_bytes()).unwrap();
-            println!("Result: {}",from_utf8(&res).unwrap());
+            let id = surreal.insert_for_id("test_table", example).unwrap();
+            println!("Result: {}",id);
         }
     }
 
     #[test]
     fn test_single(){
         let mut surreal = create_test_client();
+        let example = r#"{"kip":{"aap":"sji"},"aap":{"mies":"sjo"}}"#;
+        surreal.insert("test_table", Some("puz9ai2wrzcz52be7g04"), example.as_bytes());
         let res = surreal.get("test_table", "puz9ai2wrzcz52be7g04").unwrap();
         println!("Result: {}",from_utf8(&res).unwrap());
         let v: Value = serde_json::from_slice(&res).unwrap();
@@ -157,6 +260,35 @@ mod test {
         let id = first_object.get("id").unwrap().as_str().unwrap();
         assert_eq!("test_table:puz9ai2wrzcz52be7g04",id);
     }
+
+#[derive(Deserialize,Debug)]
+    struct City {
+        name: String
+    }
+#[test]
+    fn test_query_single(){
+ 
+        let mut surreal = create_test_client();
+        let city1 = r#"{"name":"Hanoi"}"#.as_bytes();
+        let city2 = r#"{"name":"Isesaki"}"#.as_bytes();
+        let city3 = r#"{"name":"Zeleznogorsk"}"#.as_bytes();
+        let r1 = surreal.insert_for_id("unit_query_single", city1).unwrap();
+        println!("Result: {}",r1);
+        let r2 = surreal.insert_for_id("unit_query_single", city2).unwrap();
+        println!("Result: {}",r2);
+        let r3 = surreal.insert_for_id("unit_query_single", city3).unwrap();
+        println!("Result: {}",r2);
+        let res: SurrealResult<City> = surreal.query_single("select city as name from city;").expect("huh?");
+        println!("RESULT: {:?}", res);
+        let v = res.0.iter().flat_map(|r|r.result.iter()).map(|c|c.name.as_str()).collect::<Vec<&str>>();
+        println!("RESULT2: {:?}", v);
+        assert!(v.contains(&"Zeleznogorsk"));
+        surreal.delete("unit_query_single", None);
+        let res: SurrealResult<City> = surreal.query_single("select city as name from city;").expect("huh?");
+        assert_eq!() res.0.len() 
+
+    }
+
 }
 
 
